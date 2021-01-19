f42a52674e5e0b3c151bcc73fb5046ddef0dab21
Inferus-arch <hoodarachit07@outlook.com>
2021-01-19 16:42:45 +0530

    readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# Pydb
+A Python module to work with database, based on sqlite3.
+
+### Example
+```python
+from pydb import pydb
+
+user_tabel = """
+CREATE TABLE Users
+         (ID INT PRIMARY KEY     NOT NULL,
+         NAME           TEXT    NOT NULL,
+         AGE            INT     NOT NULL);
+"""
+
+object = pydb.Pydb("filename.db", user_tabel)
+
+object.make_connection()
+object.make_table()
+
+insert_command = """
+INSERT INTO Users (ID,NAME,AGE) \
+      VALUES (1, 'Rite', 32)
+"""
+
+object.insert(insert_command)
+```
+
+### Want to contribute?
+#### [join](https://discord.gg/YpjyrMFNm7) the discord server
+
+### Future updates?
+- we will add pre defined models
+- we will provide docs
--- examples/example.py
+++ examples/example.py
@@ -2,19 +2,51 @@ from pydb import pydb
 
 # SQL Tabel syntax
 user_tabel_syntax = """
-'CREATE TABLE Users
+CREATE TABLE Users
          (ID INT PRIMARY KEY     NOT NULL,
          NAME           TEXT    NOT NULL,
          AGE            INT     NOT NULL);
 """
 
 # Pydb class object
-ExampleModel = pydb.Pydb("../example.db", user_tabel_syntax)
+ExampleModel = pydb.Pydb("example.db", user_tabel_syntax)
+
+# commands
+insert_command = """
+INSERT INTO Users (ID,NAME,AGE) \
+      VALUES (1, 'Rite', 32)
+"""
+
+select_command = """
+SELECT * from Users
+"""
+
+update_command = """
+UPDATE Users
+SET NAME = 'Rite Changed'
+WHERE ID = 1
+"""
+
+delete_command = """
+DELETE FROM Users WHERE id=1;
+"""
 
 # functions
 
 # makes connection with the file that is passed during the defination of object
 ExampleModel.make_connection()
 
-# makes the tabel, in your case it will make a user tabel
+# makes the tabel, in your case it will make a user tabel, you have to run this command only once as we have to create table only one time
 ExampleModel.make_table()
+
+# inserting value into tabel
+ExampleModel.insert(insert_command)
+
+# select something from table
+ExampleModel.select(select_command)
+
+# updates a value
+ExampleModel.update(update_command)
+
+#deletes a value
+ExampleModel.delete(delete_command)
