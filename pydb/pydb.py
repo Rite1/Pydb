d5ad0022a8c3239298e2321e39e2f71854320868
Inferus-arch <hoodarachit07@outlook.com>
2021-01-19 12:32:26 +0530

    working on it, have some basics commands done but have to improve thgem

---
+++ pydb/pydb.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+class Pydb():
+    def __init__(self, filename, tabel_sql_syntax):
+        self.filename = filename
+        self.tabel_sql_syntax = tabel_sql_syntax
+
+    def make_connection(self):
+        global connection
+        connection = sqlite3.connect(self.filename)
+        print("Made Connection")
+
+    def make_table(self):
+        connection.execute(self.tabel_sql_syntax)
+        print("Made Tabel")
+
+    def insert(self, insert_command):
+        connection.execute(insert_command)
+
+    def select(self, select_command):
+        connection.execute(select_command)
+
+    def update(self, update_command):
+        connection.execute(update_command)
